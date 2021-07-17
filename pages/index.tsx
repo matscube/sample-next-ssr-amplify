7b2e2b241a98de6e2f7a9de45f73884ebaf48b2b
Takanori Matsumoto <matscube@gmail.com>
2021-07-17 11:31:28 +0900

    chore: init as ssr app

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          sample-next-ssr-amplify
         </h1>
 
         <p className={styles.description}>
